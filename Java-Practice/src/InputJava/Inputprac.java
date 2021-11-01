83d7cc19c946187eeb52a3ce9b62f6f5934ee4f5
Md Rajib Bhuiyan <mdrajibbhuiyan71@gmail.com>
2021-11-01 19:19:56 +0600

    input in java

---
+++ Java-Practice/src/InputJava/Inputprac.java
@@ -0,0 +1,18 @@
+
+package InputJava;
+
+import java.util.Scanner;
+
+public class Inputprac {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int number;
+        System.out.print("Enter any Number : ");
+        number = input.nextInt();
+        System.out.println("Number is = " +number);
+        
+        
+        
+    }
+}
